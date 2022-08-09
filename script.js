4d6a69a87ddfad909a1ce66b8554c6f418ac8200
Nahom Mulugeta <nsolomon247@icloud.com>
2022-08-09 09:09:11 -0500

    optimization

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
 const question = document.querySelector("h3");
 const startButton = document.querySelector("#generate");
-const firstChoice = document.querySelector("#choice1");
-const secondChoice = document.querySelector("#choice2");
-const thirdChoice = document.querySelector("#choice3");
-const fourthChoice = document.querySelector("#choice4");
+const nextQuestionBtn = document.querySelector("#next-question");
+let firstChoice = document.querySelector("#choice1");
+let secondChoice = document.querySelector("#choice2");
+let thirdChoice = document.querySelector("#choice3");
+let fourthChoice = document.querySelector("#choice4");
 
+let choices = document.querySelector("ul");
 
+let questionArray = [];
 
-const getQuestions = () => fetch("https://the-trivia-api.com/api/questions")
+const getQuestions = () => fetch("https://the-trivia-api.com/api/questions?limit=10")
 .then((response) => response.json())
-.then((data) => {
-    question.innerHTML = data[0].question
-    firstChoice.innerHTML = data[0].correctAnswer
-    secondChoice.innerHTML = data[2].correctAnswer
-    thirdChoice.innerHTML = data[4].correctAnswer
-    fourthChoice.innerHTML = data[8].correctAnswer
+.then((data) => { 
+    data.forEach((i) => {
+       questionArray.push(i.question);
+    });
+       
 });
+
 
 // question.innerHTML = data[0].question
 
 startButton.addEventListener("click", () => {
     
-    getQuestions();
-
-    startButton.innerHTML = "Generate Next Question";
+    choices.innerHTML =`
+                        <ul>
+                            <button><li id = "choice1">C1</li></button>
+                            <button><li id = "choice2">C2</li></button>
+                            <break>
+                            <button><li id = "choice3">C3</li></button>
+                            <button><li id = "choice4">C4</li></button>
+                        </ul>
+                        `;
+    
+                        getQuestions();
+   
+                        question.innerHTML = questionArray[0];
     
 
+    startButton.classList.add("hidden");
+    nextQuestionBtn.classList.remove("hidden");
+
+})
+
+
+nextQuestionBtn.addEventListener("click", () => {
+    questionArray.forEach((i) =>{
+        question.innerHTML = i;
+    })
 })
 /*
 
