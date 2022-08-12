35a4312e41fc378d2e3ce51b65ee2d81dad2c6ba
Nahom Mulugeta <nsolomon247@icloud.com>
2022-08-12 07:52:45 -0500

    Organized JS, made personal qustions

--- script.js
+++ script.js
@@ -1,72 +1,130 @@
-const question = document.querySelector("h3");
-const startButton = document.querySelector("#generate");
-const nextQuestionBtn = document.querySelector("#next-question");
-let firstChoice = document.querySelector("#choice1");
-let secondChoice = document.querySelector("#choice2");
-let thirdChoice = document.querySelector("#choice3");
-let fourthChoice = document.querySelector("#choice4");
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-let choices = document.querySelector("ul");
+let shuffledQuestions, currentQuestionIndex;
 
-let questionArray = [];
 
-const getQuestions = () => fetch("https://the-trivia-api.com/api/questions?limit=10")
+const getQuestions = () => {
+fetch("https://the-trivia-api.com/api/questions?limit=10")
 .then((response) => response.json())
 .then((data) => { 
     data.forEach((i) => {
        questionArray.push(i.question);
+       corrAnswersArray.push(i.correctAnswer);
+       wrongAnswersArray.push(i.incorrectAnswers);
     });
-       
+     
 });
 
 
-// question.innerHTML = data[0].question
 
-startButton.addEventListener("click", () => {
-    
-    choices.innerHTML =`
-                        <ul>
-                            <button><li id = "choice1">C1</li></button>
-                            <button><li id = "choice2">C2</li></button>
-                            <break>
-                            <button><li id = "choice3">C3</li></button>
-                            <button><li id = "choice4">C4</li></button>
-                        </ul>
-                        `;
-    
-                        getQuestions();
-   
-                        question.innerHTML = questionArray[0];
-    
 
-    startButton.classList.add("hidden");
-    nextQuestionBtn.classList.remove("hidden");
 
+}
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
+const startGame = () =>  {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
 
-nextQuestionBtn.addEventListener("click", () => {
-    questionArray.forEach((i) =>{
-        question.innerHTML = i;
-    })
-})
-/*
+const setNextQu = es => tion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-firstChoice.addEventListener("click", () => {
-    
-  
-})
-secondChoice.addEventListener("click", () => {
-    
-    
-   
-})
-thirdChoice.addEventListener("click", () => {
-    
-    
+const showQuest = io => n(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
-})
-fourthChoice.addEventListener("click", () => {
-    
-    
- })*/
+const resetStat = e( => ) {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+const selectAns = we => r(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+const setStatus = Cl => ass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+const clearStat = us => Class(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+  {
+    question: 'Who is the best YouTuber?',
+    answers: [
+      { text: 'Web Dev Simplified', correct: true },
+      { text: 'Traversy Media', correct: true },
+      { text: 'Dev Ed', correct: true },
+      { text: 'Fun Fun const', correct = :  => true }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
+]
